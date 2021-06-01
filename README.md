814f696b6e8680f3b2c92ec7182d53e5f203b48e
stigkristiansen <stigkristiansen@outlook.com>
2021-06-01 21:37:36 +0200

    Template

---
+++ README.md
@@ -0,0 +1,6 @@
+# TwinFresh
+
+Folgende Module beinhaltet das TwinFresh Repository:
+
+- __Ventilator__ ([Dokumentation](Ventilator))  
+	Kurze Beschreibung des Moduls.
---
+++ Ventilator/module.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+	class Ventilator extends IPSModule
+	{
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			$this->RequireParent('{82347F20-F541-41E1-AC5B-A636FD3AE2D8}');
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		public function Send(string $Text, string $ClientIP, int $ClientPort)
+		{
+			$this->SendDataToParent(json_encode(['DataID' => '{C8792760-65CF-4C53-B5C7-A30FCC84FEFE}', "ClientIP" => $ClientIP, "ClientPort" => $ClientPort, "Buffer" => $Text]));
+		}
+
+		public function ReceiveData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer . ' - ' . $data->ClientIP . ' - ' . $data->ClientPort));
+		}
+	}
