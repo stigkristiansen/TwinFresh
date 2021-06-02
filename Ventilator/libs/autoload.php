47aa8034f2074bd96cd15fda683ae5a5b96f61d0
stigkristiansen <stigkristiansen@outlook.com>
2021-06-02 23:23:43 +0200

    Update autoload.php

--- Ventilator/libs/autoload.php
+++ Ventilator/libs/autoload.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 <?php
 //include_once __DIR__ . '/traits.php';
 
+IPS_LogMessage('TwinFresh', 'Inside Autoload');
+
 foreach (glob(__DIR__ . '/*.php') as $filename) {
     if (basename($filename) != 'autoload.php') {
         include_once $filename;
+        IPS_LogMessage('TwinFresh', 'Added '.$filename);
     }
 }
